f90a42da13c946cf46713c4ca7cedfe252e0337d
FarhanManshaDevops <141121756+FarhanManshaDevops@users.noreply.github.com>
2023-08-10 23:10:43 +0500

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+  # access_key = "YOUR_ACCESS_KEY"
+  # secret_key = "YOUR_SECRET_KEY"
+  # token = "YOUR_SESSION_TOKEN"
+
+  # You can also use a named profile from the AWS CLI credentials file.
+  # profile = "your_profile_name"
+}
