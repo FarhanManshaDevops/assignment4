379a45e90c63b436673e882426a6e95559f49320
FarhanManshaDevops <141121756+FarhanManshaDevops@users.noreply.github.com>
2023-08-11 15:16:38 +0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = AKIASYLSDPNESKYINC7R
-  secret_key = ceChWRTEpt4V36U42+SqGHra2Oy+DQtCg+EPJvzB
+  access_key = "AKIASYLSDPNESKYINC7R"
+  secret_key = "ceChWRTEpt4V36U42+SqGHra2Oy+DQtCg+EPJvzB"
   #token = "YOUR_SESSION_TOKEN"
 
   # You can also use a named profile from the AWS CLI credentials file.
