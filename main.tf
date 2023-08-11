84c5bcd7a0702aa4adee3610bcbce7d879934af5
FarhanManshaDevops <141121756+FarhanManshaDevops@users.noreply.github.com>
2023-08-11 17:31:47 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-#terraform {
- # backend "s3" {
-  #  bucket         = "statefilebucket"
-   # key            = "statefiles/state.tfstate"
-    #region         = "us-east-1"
-    #encrypt        = true
-   # dynamodb_table = "state_file_dynamo_db"  # Optional for locking
-  #}
-#}
+terraform {
+  backend "s3" {
+    bucket         = "statefilebucket"
+    key            = "statefilebucket/statefiles/terraform.tfstate"
+    region         = "us-east-1"            # Specify the appropriate AWS region
+    encrypt        = true                   # Optional: Set to true to enable encryption
+    dynamodb_table = "state_file_dynamo_db" # Optional: Enable locking with DynamoDB table
+  }
+}
 
 resource "aws_vpc" "assignment2-vpc" {
   cidr_block = var.vpc-cidr-block
@@ -42,32 +42,35 @@ resource "aws_security_group" "main-security-group" {
 
 resource "aws_network_acl" "NACL" {
   vpc_id = aws_vpc.assignment2-vpc.id
-  tags   = { Name = "NACL" }
+  tags = {
+  Name = "NACL" }
 }
 resource "aws_network_acl_association" "NACL-assosiation" {
   network_acl_id = aws_network_acl.NACL.id
   subnet_id      = aws_subnet.public-subnet.id
 }
 
-#resource "aws_instance" "ec2-instance" {
- # ami                         = var.instance-ami
-  #associate_public_ip_address = var.instance-public-ip-association
-  #availability_zone           = var.az-public-subnet
-  #instance_type               = var.instance-type
-  #vpc_security_group_ids      = [aws_security_group.main-security-group.id]
-  #subnet_id                   = aws_subnet.public-subnet.id
-#}
-#resource "aws_ebs_volume" "ebs-vol" {
- # availability_zone = var.az-public-subnet
-  #size              = var.ebs-size
-  #type              = "gp2"
-#}
-#resource "aws_volume_attachment" "vol-attach" {
- # device_name = "/dev/sdf" #For Linux-based AMIs, common device names are /dev/sdf, /dev/xvdf, /dev/nvme1n1, etc.
+resource "aws_instance" "ec2-instance" {
+  ami                         = var.instance-ami
+  associate_public_ip_address = var.instance-public-ip-association
+  availability_zone           = var.az-public-subnet
+  instance_type               = var.instance-type
+  vpc_security_group_ids      = [aws_security_group.main-security-group.id]
+  subnet_id                   = aws_subnet.public-subnet.id
+}
+
+resource "aws_ebs_volume" "ebs-vol" {
+  availability_zone = var.az-public-subnet
+  size              = var.ebs-size
+  type              = "gp2"
+}
+
+resource "aws_volume_attachment" "vol-attach" {
+  device_name = "/dev/sdf" #For Linux-based AMIs, common device names are /dev/sdf, /dev/xvdf, /dev/nvme1n1, etc.
   #For Windows-based AMIs, common device names are xvdf, xvdg, etc.
- # instance_id = aws_instance.ec2-instance.id
-  #volume_id   = aws_ebs_volume.ebs-vol.id
-#}
+  instance_id = aws_instance.ec2-instance.id
+  volume_id   = aws_ebs_volume.ebs-vol.id
+}
 resource "aws_s3_bucket" "mybucket" {
   #bucket = "bucket153468sd-assignment4"
   #use new bucket name everytime. otherwise code not reusable.
@@ -76,6 +79,7 @@ resource "aws_s3_bucket" "mybucket" {
     environment = "testing"
     purpose     = "for-assigment-4"
   }
-  force_destroy = false
+  force_destroy = true
 }
+
 
